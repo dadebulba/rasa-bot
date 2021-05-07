b1a5f3844065c3087c03eb7c3f76a4598d6b89fb
Davide Bulbarelli <private@dadebulba.com>
2021-05-07 18:10:28 +0200

    forms validation

--- actions/actions.py
+++ actions/actions.py
@@ -25,25 +25,18 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
-class ValidateCourse(FormValidationAction):
+
+import * from db 
+import difflib
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk import Action, Tracker
+
+class ValidateCourseTypeForm(FormValidationAction):
     """Example of a form validation action."""
 
     def name(self) -> Text:
-        return "validate_course_form"
-
-    @staticmethod
-    def cuisine_db() -> List[Text]:
-        """Database of supported cuisines."""
-
-        return [
-            "caribbean",
-            "chinese",
-            "french",
-            "greek",
-            "indian",
-            "italian",
-            "mexican",
-        ]
+        return "validate_course_type_form"
 
     @staticmethod
     def is_int(string: Text) -> bool:
@@ -55,7 +48,8 @@ class ValidateCourse(FormValidationAction):
         except ValueError:
             return False
 
-    def validate_cuisine(
+    
+    def validate_course_type(
         self,
         value: Text,
         dispatcher: CollectingDispatcher,
@@ -63,12 +57,52 @@ class ValidateCourse(FormValidationAction):
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
         """Validate cuisine value."""
-
-        if value.lower() in self.cuisine_db():
+        best_match = difflib.get_close_matches(value,db.course_types,1, cutoff=0.5)
+        if len(best_match) > 0:
             # validation succeeded, set the value of the "cuisine" slot to value
-            return {"cuisine": value}
+            return {"course_type": best_match[0]}
         else:
-            dispatcher.utter_message(response="utter_wrong_cuisine")
+            dispatcher.utter_message(response="utter_wrong_course_type")
             # validation failed, set this slot to None, meaning the
             # user will be asked for the slot again
-            return {"cuisine": None}
+            return {"course_type": None}
+class ValidateCourseChoiceForm(FormValidationAction):
+    """Example of a form validation action."""
+
+    def name(self) -> Text:
+        return "validate_course_form"
+
+    @staticmethod
+    def is_int(string: Text) -> bool:
+        """Check if a string is an integer."""
+
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+
+    
+    def validate_course(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
+        course_type = tracker.get_slot("course_type")
+        if course_type is "Laurea Triennale":
+            best_match = difflib.get_close_matches(value,db.lauree_triennali,1, cutoff=0.5)
+        elif course_type is "Laurea a Ciclo Unico":
+            best_match = difflib.get_close_matches(value,db.lauree_ciclo_unico,1, cutoff=0.5)
+        elif course_type is "Laurea Magistrale":
+            best_match = difflib.get_close_matches(value,db.lauree_magistrali,1, cutoff=0.5)
+        if len(best_match) > 0:
+            # validation succeeded, set the value of the "cuisine" slot to value
+            return {"study_course": best_match[0]}
+        else:
+            dispatcher.utter_message(response="utter_wrong_study_course")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"study_course": None}
